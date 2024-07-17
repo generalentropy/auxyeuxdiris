3ee1666062546e160104979aef306020e46e8e0c
entropie <entropie@tuta.io>
2024-07-17 16:03:30 +0200

    first commmit

---
+++ notes.md
@@ -0,0 +1,5 @@
+https://usehooks.com/uselockbodyscroll
+
+http://reactcommunity.org/react-modal/
+
+https://www.react-spring.dev/
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-gray-900 p-12 text-6xl font-bold text-white">
+      Votre expert en extensions de cils depuis 2017
+    </div>
+  );
+}
+
+export default App;
