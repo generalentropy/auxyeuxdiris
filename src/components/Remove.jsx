b6f4a8b95bfdc0e4738a18f414697ae597817226
entropie <entropie@tuta.io>
2024-07-20 10:34:12 +0200

    update

--- src/components/Remove.jsx
+++ src/components/Remove.jsx
@@ -1,5 +1,7 @@
 function Remove() {
-  return <div></div>;
+  return (
+    <div className="flex h-[600px] w-[400px] min-w-[320px] flex-col items-center rounded-3xl bg-lightGrey py-10"></div>
+  );
 }
 
 export default Remove;
