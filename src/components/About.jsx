37bb50d4c299f6b61222f6df4d366bfc4ab5f461
entropie <entropie@tuta.io>
2024-07-22 13:40:55 +0200

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,26 @@
 function About() {
   return (
-    <div className="mb-10 flex h-full max-h-[570px] rounded-2xl">
-      <div className="flex flex-col items-center justify-around rounded-l-2xl bg-accent xl:min-w-[530px]">
-        <div className="text-6xl font-bold text-white">Qui suis-je ?</div>
+    <div className="mb-10 flex max-h-[570px] flex-col sm:h-full sm:flex-row">
+      <div className="flex flex-col items-center justify-around rounded-t-2xl bg-accent py-8 sm:min-w-[50%] sm:rounded-none sm:rounded-l-2xl xl:min-w-[530px]">
+        <div className="text-4xl font-bold text-white xl:text-6xl">
+          Qui suis-je ?
+        </div>
 
         <div className="flex flex-col items-center">
-          <div className="text-4xl font-extrabold text-primary">
+          <div className="pt-6 text-2xl font-extrabold text-primary xl:text-4xl">
             José Chédotal
           </div>
-          <div className="text-3xl font-light italic text-white">
+          <div className="text-xl font-light italic text-white xl:text-3xl">
             Lash Stylist
           </div>
-          <div className="py-4 text-xl font-bold text-[#B48321]">
+          <div className="py-4 text-lg font-bold text-[#B48321] xl:text-xl">
             Cil à Cil & Volume Russe
           </div>
         </div>
 
         <InstaLink />
       </div>
-      <div className="grow overflow-hidden rounded-r-3xl">
+      <div className="grow overflow-hidden rounded-b-2xl sm:rounded-none sm:rounded-r-2xl">
         <img src="/images/jose.jpg" className="h-full w-full object-cover" />
       </div>
     </div>
@@ -31,7 +33,7 @@ function InstaLink({ link }) {
   return (
     <div className="flex">
       <a
-        className="rounded-full bg-primary px-16 py-3 text-xl font-bold text-white"
+        className="text-md rounded-full bg-primary px-8 py-2 font-bold text-white xl:px-16 xl:py-3 xl:text-xl"
         href={link ? link : "#"}
       >
         @AuxYeuxdIris
