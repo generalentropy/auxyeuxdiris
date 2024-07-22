0adc3f81d0e46f68ae284a33668a4f2519269f32
entropie <entropie@tuta.io>
2024-07-22 11:40:47 +0200

    update

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -20,11 +20,11 @@ export default Highlights;
 function Element({ title, iconUrl, subtitle }) {
   return (
     <div className="flex w-full text-primary lg:max-w-[800px]">
-      <div className="flex min-h-[100px] min-w-[100px] items-center justify-center rounded-l-2xl bg-accent lg:min-h-[150px] lg:min-w-[150px] xl:min-h-[200px] xl:min-w-[200px]">
-        <img src={iconUrl} className="h-12 lg:h-auto" />
+      <div className="flex min-h-[100px] min-w-[100px] items-center justify-center rounded-l-2xl bg-accent sm:min-h-[150px] sm:min-w-[150px] xl:min-h-[200px] xl:min-w-[200px]">
+        <img src={iconUrl} className="h-12 sm:h-16 lg:h-auto" />
       </div>
-      <div className="flex min-h-[120px] grow flex-col items-center justify-center rounded-r-3xl bg-lightGrey px-4 text-center md:py-4">
-        <div className="mb-1 text-xl font-bold lg:mb-2 lg:text-4xl">
+      <div className="flex min-h-[140px] grow flex-col items-center justify-center rounded-r-3xl bg-lightGrey px-4 py-4 text-center sm:py-4 lg:py-8">
+        <div className="mb-1 text-xl font-bold sm:mb-2 lg:text-4xl">
           {title}
         </div>
         <div className="text-sm">{subtitle}</div>
