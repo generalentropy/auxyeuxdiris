cd239b871bd69638c7f78fd97e2e61164dffcfca
entropie <entropie@tuta.io>
2024-07-24 14:58:46 +0200

    update

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,6 +1,14 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
+
 function Testimonial() {
   return (
-    <div>
+    <div className="mb-10">
       <div className="mb-10 flex items-center justify-center rounded-2xl bg-lightGrey px-6 py-12 text-xl font-bold text-primary xs:text-2xl sm:text-4xl lg:text-5xl">
         Elles{" "}
         <img
@@ -15,3 +23,46 @@ function Testimonial() {
 }
 
 export default Testimonial;
+
+function Carousel() {
+  return (
+    <div className="flex w-full items-center justify-center border border-red-500">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+          el: ".swiper-pagination",
+          type: "bullets",
+        }}
+        modules={[Pagination]}
+        className="googleTestimonial"
+        centeredSlides={true}
+        centeredSlidesBounds={true}
+        loop={true}
+        grabCursor={true}
+      >
+        <SwiperSlide className="flex items-center justify-center">
+          <div className="flex h-[300px] w-[300px] items-center justify-center rounded-2xl bg-lightGrey">
+            Slide 1
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center">
+          <div className="flex h-[300px] w-[300px] items-center justify-center rounded-2xl bg-lightGrey">
+            Slide 2
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center">
+          <div className="flex h-[300px] w-[300px] items-center justify-center rounded-2xl bg-lightGrey">
+            Slide 3
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="flex items-center justify-center">
+          <div className="flex h-[300px] w-[300px] items-center justify-center rounded-2xl bg-lightGrey">
+            Slide 4
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
