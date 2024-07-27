62c77ab3015b89a002542cc6d007fa19584107b2
entropie <entropie@tuta.io>
2024-07-27 13:20:20 +0200

    update

--- src/components/OpeningHours.jsx
+++ src/components/OpeningHours.jsx
@@ -2,8 +2,8 @@ import { FaRegCalendarCheck } from "react-icons/fa6";
 
 function OpeningHours() {
   return (
-    <div>
-      <div className="mb-10 flex items-center justify-center gap-4 rounded-2xl bg-lightGrey py-10 text-2xl font-bold text-primary sm:text-3xl lg:text-5xl">
+    <div className="text-md mb-10 flex items-center justify-center rounded-2xl bg-lightGrey py-10 text-center font-bold text-primary xs:text-lg sm:text-3xl lg:text-5xl">
+      <div className="flex items-center justify-around gap-3 border border-red-500">
         <FaRegCalendarCheck size={40} />
         Jours & horaires d’ouverture
       </div>
