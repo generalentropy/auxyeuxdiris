e6ad43b76c3ee873685b433d78e300b4e77e16d4
entropie <entropie@tuta.io>
2024-07-31 11:22:03 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,34 @@
 function Footer() {
+  const currentYear = new Date().getFullYear();
+  const iconStyle = "h-12";
+
   return (
-    <div className="mb-2 flex h-96 items-center justify-center rounded-2xl bg-accent">
-      <div>FOOTER</div>
+    <div className="mb-2 flex items-center justify-center rounded-2xl bg-accent px-2 pb-2 pt-10">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <img src="/images/logo-footer.svg" alt="" className="h-28" />
+        <div className="flex items-center justify-center gap-6">
+          <img src="icons/instagram.svg" alt="" className={iconStyle} />
+          <img src="icons/google.svg" alt="" className={iconStyle} />
+        </div>
+        <div>
+          <ul className="flex gap-4 font-semibold text-white">
+            <li>Tarifs</li>
+            <li>F.A.Q</li>
+            <li>Adresse</li>
+            <li>Horaires</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col items-center justify-center text-center text-sm text-white">
+          <p>© 2017-{currentYear} | Aux Yeux d&lsquo;Iris </p>
+          <p>
+            Entreprise Individuelle enregistrée à la Chambre des Métiers et de
+            l&lsquo;Artisanat
+          </p>
+          <p>SIRET : 829 575 703 - 47, La Plaie - 44160 Pontchâteau</p>
+        </div>
+      </div>
     </div>
   );
 }
