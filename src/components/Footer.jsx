a3db4c2acaa6ee8294b0e165f6b9f6981f0e83f1
entropie <entropie@tuta.io>
2024-07-31 17:44:08 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
 function Footer() {
   const currentYear = new Date().getFullYear();
-  const iconStyle = "h-12";
+  const iconStyle = "lg:h-12 h-10";
 
   return (
-    <div className="mb-2 flex items-center justify-center rounded-2xl bg-accent px-2 pb-2 pt-10">
+    <div className="mb-2 flex items-center justify-center rounded-2xl bg-accent px-2 pb-2 pt-5">
       <div className="flex flex-col items-center justify-center gap-4">
-        <img src="/images/logo-footer.svg" alt="" className="h-28" />
+        <img src="/images/logo-footer.svg" alt="" className="h-20 lg:h-28" />
         <div className="flex items-center justify-center gap-6">
           <img src="icons/instagram.svg" alt="" className={iconStyle} />
           <img src="icons/google.svg" alt="" className={iconStyle} />
         </div>
         <div>
-          <ul className="flex gap-4 font-semibold text-white">
+          <ul className="flex gap-3 text-xs font-semibold text-white">
             <li>Tarifs</li>
             <li>F.A.Q</li>
             <li>Adresse</li>
@@ -20,7 +20,7 @@ function Footer() {
           </ul>
         </div>
 
-        <div className="flex flex-col items-center justify-center text-center text-sm text-white">
+        <div className="flex flex-col items-center justify-center px-2 text-center text-xs text-white">
           <p>© 2017-{currentYear} | Aux Yeux d&lsquo;Iris </p>
           <p>
             Entreprise Individuelle enregistrée à la Chambre des Métiers et de
