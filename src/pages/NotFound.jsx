41e15b5766992b619eefbc6d2778653c071a5d00
entropie <entropie@tuta.io>
2024-08-02 23:57:33 +0200

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,18 @@
 function NotFound() {
-  return <div>404</div>;
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-3 text-primary">
+      <p className="text-8xl font-bold">404</p>
+
+      <p>
+        <img src="images/j.png" />
+      </p>
+      <p className="py-2">Ben euh... comment</p>
+      <p>
+        Je crois que t&lsquo;as pas dû taper la bonne adresse car la page elle
+        existe pas.
+      </p>
+    </div>
+  );
 }
 
 export default NotFound;
