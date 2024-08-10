0247c54888c78145dcb478be20a67cdc66dd4a3f
entropie <entropie@tuta.io>
2024-08-10 12:11:01 +0200

    update

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -25,12 +25,14 @@ function Modal({ title, content, isOpen, callback }) {
       className="fixed z-50 flex h-screen w-screen items-center justify-center bg-primary bg-opacity-80"
       onClick={handleClose}
     >
-      <div className="relative m-2 flex w-full max-w-[700px] items-center justify-center rounded-2xl border-8 border-white bg-lightGrey px-2 pb-5 pt-10 text-primary xs:px-3 md:px-12 md:pt-20">
+      <div className="relative m-2 flex w-full max-w-[700px] items-center justify-center rounded-2xl border-8 border-white bg-lightGrey px-3 pb-5 pt-10 text-primary xs:px-4 md:px-10 md:py-20">
         <div className="flex flex-col gap-2 md:gap-3">
           <p className="text-2xl font-bold text-primary md:text-5xl">
             {displayTitle}
           </p>
-          <p className="text-primary md:text-lg">{displayContent}</p>
+          <p className="text-sm text-primary xs:text-base md:text-lg">
+            {displayContent}
+          </p>
         </div>
         <IoCloseCircle
           className="absolute right-2 top-2 cursor-pointer text-accent transition-colors hover:text-accentDarker lg:right-5 lg:top-5"
